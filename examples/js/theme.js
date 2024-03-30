5b8d56da4ffdf050bf8cc9b482b771e1e0374755
Oleh <oleh.obydonov@inbase.com.ua>
2024-03-30 17:47:51 +0200

    *

---
+++ examples/js/theme.js
@@ -0,0 +1,15 @@
+/**
+ * Sets the default theme for the webpage.
+ */
+function setDefaultTheme() {
+  const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+  if (darkThemeMq.matches) {
+    document.documentElement.setAttribute('data-theme', 'dark')
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light')
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  setDefaultTheme()
+})
